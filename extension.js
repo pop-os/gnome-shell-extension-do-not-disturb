e4399caf22a2ea83f59619c95fff27f8f151c8b0
Kyle Corry <kylecorry31@gmail.com>
2018-03-17 09:22:32 -0400

    Properly align do not distub toggle

--- extension.js
+++ extension.js
@@ -4,26 +4,38 @@ const Ornament = imports.ui.popupMenu.Ornament;
 const Util = imports.misc.util;
 const St = imports.gi.St;
 const Gio = imports.gi.Gio;
-// const Gtk = imports.gi.Gtk;
+const Clutter = imports.gi.Clutter;
 
 function init() {}
 
 function enable() {
-    this.mainMenu = Main.panel.statusArea['dateMenu'].menu;
+    this.clearButton = Main.panel.statusArea.dateMenu._messageList._clearButton;
 
-    this.calendarBox = Main.panel.statusArea.dateMenu._messageList._clearButton.get_parent();
+    this.calendarBox = this.clearButton.get_parent();
+
+    this.clearBox = new St.BoxLayout({ vertical: false,
+                                       x_expand: true,
+                                       y_expand: false });
+
 
     this.disturbToggle = new PopupMenu.PopupSwitchMenuItem("Do not disturb");
 
+    this.disturbToggle.actor.add_style_class_name('do-not-disturb');
+    this.disturbToggle.actor.set_x_expand(true);
 
     this.disturbToggle.connect("toggled", (item, event) => {
       this.set_do_not_disturb(event);
     });
 
-    // this.disturbToggle.actor.set_x_align(Clutter.ActorAlign.START);
-    this.calendarBox.add_actor(this.disturbToggle.actor);
+    this.disturbToggle.actor.set_x_align(Clutter.ActorAlign.START);
 
-    // this.mainMenu.addMenuItem(this.disturbSwitch);
+    this.clearButton.set_height(this.disturbToggle.actor.get_height());
+    this.clearBox.add_actor(this.disturbToggle.actor);
+    this.clearButton.reparent(this.clearBox);
+    this.clearButton.add_style_class_name('clear-button');
+
+    this.calendarBox.add_actor(this.clearBox);
+
     this.disturbToggle.setToggleState(is_do_not_disturb());
 }
 
